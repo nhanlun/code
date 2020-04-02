b0646284c71ce3941fd86263c35b4b456fd76732
nhanlun <ntnhan18@apcs.vn>
2020-04-02 22:49:10 +0700

    Nothing new

--- C++/try.cpp
+++ C++/try.cpp
@@ -2,23 +2,7 @@
 
 using namespace std;
 
-class Object
-{
-private:
-    Object() = default;
-    ~Object() = default;
-public:
-    static Object& getInstance()
-    {
-        static Object instance;
-        return instance;
-    }
-    Object(const Object&) = delete;
-    Object(Object&&) = delete;
-};
-
 int main()
 {
-    Object::getInstance();
     return 0;
 }
